0298b099e31e8a7046deb5d7120c9edb16f17e0f
Aditi Rai <aditi45@gmail.com>
2024-10-02 09:37:15 -0400

    updated connection.ts

--- src/connection.ts
+++ src/connection.ts
@@ -8,7 +8,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     host: 'localhost',
     database: process.env.DB_NAME,
     port: 5432,

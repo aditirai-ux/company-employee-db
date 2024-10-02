2ec66ae7bd067fc3f9382446f7bd634060b04e17
Aditi Rai <aditi45@gmail.com>
2024-10-01 22:17:43 -0400

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,14 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department (id)
     ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+    ON DELETE CASCADE
+);
